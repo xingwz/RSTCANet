100701eca85d53e2d03b01bd163fc2cf1ae134b3
Wenzhu Xing <xwzlife@yahoo.com>
2022-04-12 16:59:42 +0300

    test code clean

--- main_test_dm.py
+++ main_test_dm.py
@@ -5,7 +5,7 @@ Created on Thu Oct 14 11:26:36 2021
 
 @author: xingw
 """
-
+import argparse
 import os.path
 import logging
 
@@ -51,42 +51,44 @@ def main():
     # ----------------------------------------
     # Preparation
     # ----------------------------------------
-
-    model_name = 'RSTCANet_L'           # 'RSTCANet_B' | 'RSTCANet_S' | 'RSTCANet_L'
-    testset_name = 'McM'               # test set,  'McM' | 'kodak' | 'CBSD68' | 'urban100'
-    need_degradation = True              # default: True
-    show_img = False                     # default: False
-
-
-
-
-    task_current = 'dm_model_zoo'
-    pattern = 1                    # unused for Bayer pattern
-    n_channels = 3        # setting for color image
-    nc = 128               # setting for color image
-    window_size=8
-    num_heads=[8, 8, 8, 8]
-    depths = [8, 8, 8, 8]
-    patch_size = 2               # setting for color image
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default='RSTCANet_B', help='RSTCANet_B or RSTCANet_S or RSTCANet_L')
+    parser.add_argument('--testset_name', type=str, default='McM', help='McM or kodak or CBSD68 or urban100')
+    parser.add_argument('--need_degradation', type=bool, default=True, help='Data preprocessing')
+    parser.add_argument('--show_img', type=bool, default=False, help='Show the mosaiced, demosaiced and GT images')
+    parser.add_argument('--task_current', type=str, default='dm_model_zoo', help='dm_model_zoo')
+    parser.add_argument('--pattern', type=int, default=1, help='1 (RGGB) or 2 (GRBG) or 3 (GBRG) or 4 (BGGR)')
+    parser.add_argument('--n_channels', type=int, default=3, help='setting for color image')
+    parser.add_argument('--nc', type=int, default=72, help='channel number')
+    parser.add_argument('--window_size', type=int, default=8, help='window size of Swin Transformer')
+    parser.add_argument('--num_heads', type=int, default=6, help='window size of Swin Transformer')
+    parser.add_argument('--K', type=int, default=6, help='number of STLs in one RSTCAB')
+    parser.add_argument('--N', type=int, default=2, help='number of RSTCAB')
+    parser.add_argument('--patch_size', type=int, default=2, help='patch size for patch partition')
+    parser.add_argument('--sf', type=int, default=0, help='scale factor')
     
-    sf = 1
+    args = parser.parse_args()
+
+    num_heads=[args.num_heads for i in range(args.N)]
+    depths = [args.K for i in range(args.N)]
     
-    if 'clip' in model_name:
+    sf = args.sf
+    
+    if 'clip' in args.model_name:
         use_clip = True       # clip the intensities into range of [0, 1]
     else:
         use_clip = False
-    model_pool = 'models'  # fixed
     testsets = 'testset'     # fixed
     results = 'results'       # fixed
-    result_name = testset_name + '_' + model_name
-    border = sf if task_current == 'sr' else 0     # shave boader to calculate PSNR and SSIM
-    model_path = os.path.join(task_current, model_pool, model_name+'.pth')
+    result_name = args.testset_name + '_' + args.model_name
+    border = sf if args.task_current == 'sr' else 0     # shave boader to calculate PSNR and SSIM
+    model_path = os.path.join(args.task_current, args.model_name+'.pth')
 
     # ----------------------------------------
     # L_path, E_path, H_path
     # ----------------------------------------
 
-    L_path = os.path.join(testsets, testset_name) # L_path, for Low-quality images
+    L_path = os.path.join(testsets, args.testset_name) # L_path, for Low-quality images
     H_path = L_path                               # H_path, for High-quality images
     E_path = os.path.join(results, result_name)   # E_path, for Estimated images
     util.mkdir(E_path)
@@ -105,8 +107,8 @@ def main():
     # ----------------------------------------
 
     from models.network_dm import RSTCANet as net
-    model = net(in_nc=1, out_nc=n_channels, patch_size=patch_size, nc=nc, 
-                window_size=window_size, num_heads=num_heads, depths=depths)
+    model = net(in_nc=1, out_nc=args.n_channels, patch_size=args.patch_size, nc=args.nc, 
+                window_size=args.window_size, num_heads=num_heads, depths=depths)
     model.load_state_dict(torch.load(model_path), strict=True)
     model.eval()
     for k, v in model.named_parameters():
@@ -118,7 +120,7 @@ def main():
     test_results['psnr'] = []
     test_results['ssim'] = []
 
-    logger.info('model_name:{}'.format(model_name))
+    logger.info('model_name:{}'.format(args.model_name))
     logger.info(L_path)
     L_paths = util.get_image_paths(L_path)
     H_paths = util.get_image_paths(H_path) if need_H else None
@@ -131,18 +133,18 @@ def main():
 
         img_name, ext = os.path.splitext(os.path.basename(img))
         # logger.info('{:->4d}--> {:>10s}'.format(idx+1, img_name+ext))
-        img_L = util.imread_uint(img, n_channels=n_channels)
+        img_L = util.imread_uint(img, n_channels=args.n_channels)
 
         if need_degradation:  # degradation process
 #            img_L, h_border, w_border = util.image_padding(img_L, 16*4)
             img_L = util.modcrop(img_L, 16)
-            img_L = util.mosaic_CFA_Bayer(img_L, pattern)
+            img_L = util.mosaic_CFA_Bayer(img_L, args.pattern)
             # # demosaicing
             # img_L = util.demosaic(img_L, pattern)
             if use_clip:
                 img_L = util.uint2single(img_L)
 
-        util.imshow(img_L, title='Mosaic image') if show_img else None
+        util.imshow(img_L, title='Mosaic image') if args.show_img else None
 
         img_L = util.uint2tensor4(img_L)
         img_L = img_L.to(device)
@@ -160,7 +162,7 @@ def main():
             # --------------------------------
             # (3) img_H
             # --------------------------------
-            img_H = util.imread_uint(H_paths[idx], n_channels=n_channels)
+            img_H = util.imread_uint(H_paths[idx], n_channels=args.n_channels)
             img_H = util.modcrop(img_H, 16)
             img_H = img_H.squeeze()
 
@@ -173,7 +175,7 @@ def main():
             test_results['psnr'].append(psnr)
             test_results['ssim'].append(ssim)
             logger.info('{:s} - PSNR: {:.2f} dB; SSIM: {:.4f}.'.format(img_name+ext, psnr, ssim))
-            util.imshow(np.concatenate([img_E, img_H], axis=1), title='Recovered / Ground-truth') if show_img else None
+            util.imshow(np.concatenate([img_E, img_H], axis=1), title='Recovered / Ground-truth') if args.show_img else None
 
         # ------------------------------------
         # save results
